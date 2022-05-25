db2e9f9bf432e95d5212aec77bb0188dd049b206
Gokul Raj <106130828+Gokulcasper@users.noreply.github.com>
2022-05-25 18:46:23 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,43 @@
+// Datatypes in javascripts
+/*  JS -> Dynamic programming
+
+string
+number
+boolean
+null
+undefined 
+symbols
+
+reference datatypes
+
+array
+object literal
+date and time
+
+*/
+
+var Name = "gokul";
+console.log(typeof Name);
+var a = 20;
+console.log(a);
+var isgood = true;
+console.log(isgood);
+var value = null;
+console.log(value);
+let b;
+console.log(b);
+const s1 = Symbol();
+console.log(s1);
+const s2 = Symbol();
+console.log(s2);
+console.log(s1 == s2);
+
+var data = ["c", "c++", "java"];
+console.log(data);
+var stud = {
+  Name: "gokul",
+  Age: 25,
+};
+console.log(typeof stud);
+var d = new Date();
+console.log(d);
