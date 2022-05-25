ec5a8c0413fe106166010fb785875fa206524ad6
Gokul Raj <106130828+Gokulcasper@users.noreply.github.com>
2022-05-25 17:18:18 +0530

    Add files via upload

---
+++ var.js
@@ -0,0 +1,54 @@
+// 2. variable Redeclaration
+/*
+var a = 20;
+console.log(a);
+
+var a = 25;
+console.log(a);
+*/
+
+/*
+let a = 20;
+console.log(a);
+
+let a = 25;
+console.log(a);
+*/
+
+/*
+const a = 20;
+console.log(a);
+
+const a = 25;
+console.log(a);
+*/
+
+// 3. value assignment
+/*
+var a = 20;
+console.log(a);
+a = 25;
+console.log(a);
+*/
+
+/*
+let a = 20;
+console.log(a);
+
+a = 25;
+console.log(a);
+*/
+
+/*
+const a = 20;
+console.log(a);
+
+a = 25;
+console.log(a);
+*/
+
+const student = { name: "Gokul", Age: 25 };
+console.table(student);
+console.log(student.name);
+student.name = "raj";
+console.table(student);
