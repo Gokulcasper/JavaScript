390a2bd3d5ee20174ff61f66d8ffa5d0f090c1e0
Gokul Raj <106130828+Gokulcasper@users.noreply.github.com>
2022-05-25 15:51:00 +0530

    Add files via upload

---
+++ key.js
@@ -0,0 +1,19 @@
+/*
+var
+let
+const
+*/
+
+var a = 10;
+var b = 20;
+console.log(a + b);
+// 1. scope
+
+if (true) {
+  // var msg = "Welcome to JS";
+  /*  let msg = "Welcome to JS";
+  console.log(msg);  */
+  const msg = "Welcome to JS";
+  console.log(msg);
+}
+//console.log(msg);
